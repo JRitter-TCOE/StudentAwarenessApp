d46162397b6eeeb1776f0d982709fb70243e12f5
JRitter-TCOE <jritter@tcoek12.org>
2024-01-26 16:12:58 -0800

    Adjusting styles

--- web-app/pages/report.php
+++ web-app/pages/report.php
@@ -10,7 +10,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   </head>
-  <body id="login">
+  <body id="report">
 
     
     <img id="bg_img" src="../images/Background_3.jpg">
