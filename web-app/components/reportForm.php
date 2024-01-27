ff98a51b660432dc200395f6439a6e921413d5cb
JRitter-TCOE <jritter@tcoek12.org>
2024-01-26 16:28:31 -0800

    Adjusting report form

--- web-app/components/reportForm.php
+++ web-app/components/reportForm.php
@@ -1,5 +1,10 @@
 <h1>First Responder<br>HOSWC Notification</h1>
 <form id='report_form'>
+
+  <h2>Incident Details (Required)</h2>
+
+  <hr>
+
   <label for='inc_date'>DATE OF INCIDENT (MM/DD/YYYY):</label>
   <input id='inc_date' name='inc_date' type='date'>
 
@@ -11,25 +16,33 @@
   echo "<input id='agency' name='agency' value='".$_SESSION['username']."'>";
   ?>
 
+  <h2>Child Details (Required)</h2>
+
+  <hr>
+
+  <label for='child_fname'>CHILD FIRST NAME:</label>
+  <input id='child_fname' name='child_fname' placeholder='Child First Name'>   
+
+  <label for='child_lname'>CHILD LAST NAME:</label>
+  <input id='child_lname' name='child_lname' placeholder='Child Last Name'>
+  
+  
+  <label for='school_select'>SCHOOL:</label>
+  <?php include('../components/schoolSelect.html') ?>
+
+  <h2>Optional Details</h2>
+
+  <hr>
+
+  <label for='child_dob'>CHILD DATE OF BIRTH (MM/DD/YYYY):</label>
+  <input id='child_dob' name='child_dob' type='date'>
+
   <label for='reported_by'>OFFICER / REPORTER NAME:</label>
   <input id='reported_by' name='reported_by' placeholder='Officer / Reporter Name'>
 
   <label for='case_num'>CASE NUMBER:</label>
   <input id='case_num' name='case_num' placeholder='Case Number'>
-  
-  <hr>
 
-  <label for='child_fname'>CHILD FIRST NAME:</label>
-  <input id='child_fname' name='child_fname' placeholder='Child First Name'>   
-  <label for='child_lname'>CHILD LAST NAME:</label>
-  <input id='child_lname' name='child_lname' placeholder='Child Last Name'>
-  
-  <label for='child_dob'>DATE OF BIRTH (MM/DD/YYYY):</label>
-  <input id='child_dob' name='child_dob' type='date'>
-
-  <label for='school_select'>SCHOOL:</label>
-  <?php include('../components/schoolSelect.html') ?>
-  
   <hr>
 
   <p class='feedback' id='report_feedback'></p>
