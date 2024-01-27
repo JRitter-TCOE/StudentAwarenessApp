6c28b6b13510d52161129bedf3c6d6000d544d6e
JRitter-TCOE <jritter@tcoek12.org>
2024-01-26 17:14:37 -0800

    adjusting styles

--- web-app/components/navbar.php
+++ web-app/components/navbar.php
@@ -2,15 +2,19 @@
   <div id="topBar">
   <img id="nav_logo" src="../images/logo.png">
     <div id="info">
-      <span>Welcome:&nbsp;East&nbsp;Weaver</span>
-      <span id="current_user"></span>
-      <span>Access:&nbsp;School</span>
-      <span id="access_type"></span>
-      <a id="logout" href="../index.php">Logout</a>
+      <span>Welcome:</span>
+      <span id="current_user">East&nbsp;Weaver</span>
+      <span>|</span>
+      <span>Access:</span>
+      <span id="access_type">School</span>
+      <span>|</span>
       <ion-icon name="call"></ion-icon>
       <a href="tel:5306232861">530-623-2861</a>
+      <span>|</span>
       <ion-icon name="mail"></ion-icon>
       <a href="mailto:hoswc@tcoek12.org">hoswc@tcoek12.org</a>
+      <span>|</span>
+      <a id="logout" href="../index.php">Logout</a>
     </div>
   </div>
   <div id="tabs">
