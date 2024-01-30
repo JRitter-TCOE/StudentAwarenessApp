51b64cb8200ba30f7e91e3f690073423d4baadd9
JRitter-TCOE <jritter@tcoek12.org>
2024-01-29 16:11:37 -0800

    Changing login.php to query db

--- web-app/api/login.php
+++ web-app/api/login.php
@@ -2,25 +2,44 @@
 
 session_start();
 
-$agency_user = 'wpd';
-$agency_pass = 'root';
+// OLD METHOD
 
-$school_user = 'EastW';
-$school_pass = 'root';
+// $agency_user = 'wpd';
+// $agency_pass = 'root';
+
+// $school_user = 'EastW';
+// $school_pass = 'root';
 
 
-if ($_POST['username'] == $agency_user && $_POST['password'] == $agency_pass) {
-  echo json_encode(array("data"=>"SUCCESS"));
-  $_SESSION['username'] = $_POST['username'];
+// if ($_POST['username'] == $agency_user && $_POST['password'] == $agency_pass) {
+//   echo json_encode(array("data"=>"SUCCESS"));
+//   $_SESSION['username'] = $_POST['username'];
+// }
+// else if ($_POST['username'] == $school_user && $_POST['password'] == $school_pass) {
+//     echo json_encode(array("data"=>"SUCCESS"));
+//     $_SESSION['username'] = $_POST['username'];
+//   }
+// else {
+//   echo json_encode(array("data"=>"FAILURE"));
+// }
+
+
+
+// NEW METHOD
+
+include('./db_connection.php');
+
+$table = 'Login';
+
+$username = $_POST['username'];
+
+foreach($db->query("SELECT UserPass FROM $table where UserName = $username") as $row) {
+    if ($row['UserPass'] == $_POST['password']) {
+      echo json_encode(array("data"=>"SUCCESS"));
+    }
+    else {
+      echo json_encode(array("data"=>"FAILURE"));
+    }
+    break;
 }
-else if ($_POST['username'] == $school_user && $_POST['password'] == $school_pass) {
-    echo json_encode(array("data"=>"SUCCESS"));
-    $_SESSION['username'] = $_POST['username'];
-  }
-else {
-  echo json_encode(array("data"=>"FAILURE"));
-}
-
-
-
 ?>
