84e6dd8094549c3b84885221823687e66c4a129d
JRitter-TCOE <jritter@tcoek12.org>
2024-01-30 16:18:33 -0800

    adjusting nav

--- web-app/components/navbar.php
+++ web-app/components/navbar.php
@@ -2,22 +2,24 @@
   <div id="topBar">
   <img id="nav_logo" src="../images/logo.png">
     <div id="info">
-      <span>Welcome:</span>
-      <span id="current_user">East&nbsp;Weaver</span>
-      <span>|</span>
-      <span>Access:</span>
-      <span id="access_type">School</span>
-      <span>|</span>
-      <ion-icon name="call"></ion-icon>
-      <a href="tel:5306232861">530-623-2861</a>
-      <span>|</span>
-      <ion-icon name="mail"></ion-icon>
-      <a href="mailto:hoswc@tcoek12.org">hoswc@tcoek12.org</a>
-      <span>|</span>
-      <a id="logout" href="../index.php">Logout</a>
-    </div>
-    <div id="info_menu_btn">
-      <ion-icon name="menu-outline" size="large"></ion-icon>
+      <div id="long_info">
+        <span>Welcome:</span>
+        <span id="current_user">East&nbsp;Weaver</span>
+        <span>|</span>
+        <span>Access:</span>
+        <span id="access_type">School</span>
+        <span>|</span>
+        <ion-icon name="call"></ion-icon>
+        <a href="tel:5306232861">530-623-2861</a>
+        <span>|</span>
+        <ion-icon name="mail"></ion-icon>
+        <a href="mailto:hoswc@tcoek12.org">hoswc@tcoek12.org</a>
+        <span>|</span>
+        <a id="logout" href="../index.php">Logout</a>
+      </div>
+      <div id="short_info">
+        <ion-icon name="menu-outline" size="large"></ion-icon>
+      </div>
     </div>
   </div>
   <div id="tabs">
