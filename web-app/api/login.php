5f64fb9c16a0e279cff6257ebf365ea18cc0dd37
JRitter-TCOE <jritter@tcoek12.org>
2024-01-31 16:55:14 -0800

    testing login

--- web-app/api/login.php
+++ web-app/api/login.php
@@ -12,7 +12,7 @@ $table = 'Login';
 
 $username = $_POST['username'];
 
-foreach($db->query("SELECT UserPass FROM $table where UserName = '$username'") as $row) {
+foreach($db->query("SELECT UserPass, Role FROM $table where UserName = '$username'") as $row) {
     if ($row['UserPass'] == $_POST['password']) {
       echo json_encode(array("data"=>"SUCCESS"));
       $_SESSION['username'] = $_POST['username'];
