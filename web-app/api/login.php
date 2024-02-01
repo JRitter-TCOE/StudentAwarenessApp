ab4c0a416ee429e23453b672c464b9ceb6da72c4
JRitter-TCOE <jritter@tcoek12.org>
2024-01-31 17:13:41 -0800

    testing login

--- web-app/api/login.php
+++ web-app/api/login.php
@@ -9,16 +9,18 @@ error_reporting(E_ALL);
 include('./db_connection.php');
 
 $table = 'Login';
-
 $username = $_POST['username'];
 
-$result = $db->query("SELECT UserPass, Role, OrgID FROM $table where UserName = '$username'")[0];
 
-if ($result['UserPass'] == $_POST['password']) {
+$stmt = $db->prepare("SELECT UserPass, Role, OrgID FROM $table where UserName = $username");
+$stmt->execute();
+$row = $stmt->fetch();
+
+if ($row['UserPass'] == $_POST['password']) {
   echo json_encode(array("data"=>"SUCCESS"));
   $_SESSION['username'] = $_POST['username'];
-  $_SESSION['role'] = $result['Role'];
-  $_SESSION['orgID'] = $result['OrgID'];
+  $_SESSION['role'] = $row['Role'];
+  $_SESSION['orgID'] = $row['OrgID'];
 }
 else {
   echo json_encode(array("data"=>"FAILURE"));
