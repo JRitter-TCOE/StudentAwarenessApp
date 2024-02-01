4443061bab1a907a6925fa25c31ab7d343c1b50e
JRitter-TCOE <jritter@tcoek12.org>
2024-01-31 17:08:59 -0800

    testing login

--- web-app/api/login.php
+++ web-app/api/login.php
@@ -12,16 +12,16 @@ $table = 'Login';
 
 $username = $_POST['username'];
 
-foreach($db->query("SELECT UserPass, Role, OrgID FROM $table where UserName = '$username'") as $row) {
-    if ($row['UserPass'] == $_POST['password']) {
-      echo json_encode(array("data"=>"SUCCESS"));
-      $_SESSION['username'] = $_POST['username'];
-      $_SESSION['role'] = $row['Role'];
-      $_SESSION['orgID'] = $row['OrgID'];
-    }
-    else {
-      echo json_encode(array("data"=>"FAILURE"));
-    }
-    break;
+$result = $db->query("SELECT UserPass, Role, OrgID FROM $table where UserName = '$username'")[0];
+
+if ($result['UserPass'] == $_POST['password']) {
+  echo json_encode(array("data"=>"SUCCESS"));
+  $_SESSION['username'] = $_POST['username'];
+  $_SESSION['role'] = $result['Role'];
+  $_SESSION['orgID'] = $result['OrgID'];
 }
+else {
+  echo json_encode(array("data"=>"FAILURE"));
+}
+
 ?>
