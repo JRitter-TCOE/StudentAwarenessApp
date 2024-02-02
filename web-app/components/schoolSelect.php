2edc9655a28fbb3f57cb7017eebeeb2ee02930a9
JRitter-TCOE <jritter@tcoek12.org>
2024-02-01 16:49:33 -0800

    testing select

--- web-app/components/schoolSelect.php
+++ web-app/components/schoolSelect.php
@@ -1,10 +1,5 @@
 <?php 
-session_start();
 include('../api/db_connection.php');
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 ?>
 
 <select id='school_select'>
