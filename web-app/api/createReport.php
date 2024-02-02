8b7377ba807a1d0549679f58ec89c60566ba223f
JRitter-TCOE <jritter@tcoek12.org>
2024-02-01 16:45:40 -0800

    Testing select

--- web-app/api/createReport.php
+++ web-app/api/createReport.php
@@ -3,8 +3,47 @@
 session_start();
 
 
-echo $_POST['child_fname'];
-echo $_POST['child_lname'];
+$inc_date = $_POST['inc_date'];
+$inc_time = $_POST['inc_time'];
+$agency = $_POST['agency'];
+$reported_by = $_POST['reported_by'];
+$case_num = $_POST['case_num'];
+$child_fname = $_POST['child_fname'];
+$child_lname = $_POST['child_lname'];
+$child_dob = $_POST['child_dob'];
+$school = $_POST['school'];
 
+
+include("./db_connection.php");
+
+$table = "Students";
+
+$stmt = $db->prepare("INSERT INTO $table (
+    SchoolID,
+    FirstName,
+    LastName,
+    DateOfBirth,
+    IncidentDate,
+    IncidentTime,
+    ReportingAgency,
+    OfficerName,
+    CaseNumber,
+    Status 
+    )
+    VALUES (
+        $school,
+        '$child_fname',
+        '$child_lname',
+        '$child_dob',
+        '$inc_date',
+        '$inc_time',
+        '$agency',
+        '$reported_by',
+        '$case_num',
+        0
+    )
+");
+
+$stmt->execute();
 
 ?>
---
+++ web-app/components/schoolSelect.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();
+include('../api/db_connection.php');
+?>
+
+<select id='school_select'>
+  <option value="">--Please Select School--</option>
+  <?php 
+
+  $table = "Schools";
+
+  foreach($db->query("SELECT SchoolID, SchoolName FROM $table") as $row) {
+    $value = $row["SchoolID"];
+    $name = $row["SchoolName"]
+
+    echo "<option value=$value>$name</option>";
+  }
+
+  ?>
+</select>
