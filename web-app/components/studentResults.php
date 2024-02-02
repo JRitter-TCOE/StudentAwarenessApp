247375aad83387525769c6278d1f74f2ce13c882
JRitter-TCOE <jritter@tcoek12.org>
2024-02-02 15:49:00 -0800

    Testing studentResults

--- web-app/components/studentResults.php
+++ web-app/components/studentResults.php
@@ -1,42 +1,58 @@
 <?php 
-$testData = [
-  array(
-    "status" => 0, 
-    "date" => "01-20-2024",
-    "child_fname" => "Billy",
-    "child_lname" => "Kidd",
-    "dob" => "04-28-2010",
-    "school" => "East Weaver"
-  ),
-  array(
-    "status" => 1, 
-    "date" => "01-17-2024",
-    "child_fname" => "Sally",
-    "child_lname" => "Field",
-    "dob" => "02-14-2016",
-    "school" => "East Weaver"
-  ),
-  array(
-    "status" => 1, 
-    "date" => "01-12-2024",
-    "child_fname" => "John",
-    "child_lname" => "Smith",
-    "dob" => "07-03-2014",
-    "school" => "East Weaver"
-  ),
-  array(
-    "status" => 1, 
-    "date" => "01-10-2024",
-    "child_fname" => "Billy",
-    "child_lname" => "Kidd",
-    "dob" => "04-28-2010",
-    "school" => "East Weaver"
-  )
+
+
+// $testData = [
+//   array(
+//     "status" => 0, 
+//     "date" => "01-20-2024",
+//     "child_fname" => "Billy",
+//     "child_lname" => "Kidd",
+//     "dob" => "04-28-2010",
+//     "school" => "East Weaver"
+//   ),
+//   array(
+//     "status" => 1, 
+//     "date" => "01-17-2024",
+//     "child_fname" => "Sally",
+//     "child_lname" => "Field",
+//     "dob" => "02-14-2016",
+//     "school" => "East Weaver"
+//   ),
+//   array(
+//     "status" => 1, 
+//     "date" => "01-12-2024",
+//     "child_fname" => "John",
+//     "child_lname" => "Smith",
+//     "dob" => "07-03-2014",
+//     "school" => "East Weaver"
+//   ),
+//   array(
+//     "status" => 1, 
+//     "date" => "01-10-2024",
+//     "child_fname" => "Billy",
+//     "child_lname" => "Kidd",
+//     "dob" => "04-28-2010",
+//     "school" => "East Weaver"
+//   )
   
-];
+// ];
+
+include('../api/db_connection.php');
+
+$schoolID = $_SESSION['orgID'];
+
+$data = $db->query('SELECT 
+  Status, 
+  IncidentDate, 
+  FirstName, 
+  LastName, 
+  DateOfBirth
+  FROM Students
+  WHERE SchoolID = $schoolID
+')
 
 
-foreach ($testData as $entry) {
+foreach ($data as $entry) {
 
   $status = '<ion-icon class="notification_handled" name="checkmark-circle" size="large" ></ion-icon>';
   $button = '';
@@ -46,11 +62,12 @@ foreach ($testData as $entry) {
     $button = '<button class="confirm_btn"><ion-icon name="checkbox-outline" size="large"></ion-icon></button>';
   }
 
-  $date = $entry['date'];
-  $fname = $entry['child_fname'];
-  $lname = $entry['child_lname'];
-  $dob = $entry['dob'];
-  $school = $entry['school'];
+
+  $date = $entry['IncidentDate'];
+  $fname = $entry['FirstName'];
+  $lname = $entry['LastName'];
+  $dob = $entry['DateOfBirth'];
+  $school = $_SESSION['schoolName'];
 
   echo "<div class='row entry'>
     <p class='field_small'>$status</p>
