24835b81f9c4bdc2913900ff4f1be5056eb4d3c5
JRitter-TCOE <jritter@tcoek12.org>
2024-02-02 16:48:46 -0800

    testing confirm btn

--- web-app/api/confirmStudentHandled.php
+++ web-app/api/confirmStudentHandled.php
@@ -7,7 +7,7 @@ try {
     
     include('./db_connection.php');
     
-    $stmt = $db->prepare("UPDATE Students SET Status = 1 WHERE StudentID = '$studentID'");
+    $stmt = $db->prepare("UPDATE Students SET Status = 1 WHERE StudentID = $studentID");
     $stmt->execute();
 
     echo json_encode(array('data'=>'SUCCESS'));
