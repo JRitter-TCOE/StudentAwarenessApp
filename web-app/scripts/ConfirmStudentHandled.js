01f94db2f660b3064c7f397a7f6f080695c6280b
JRitter-TCOE <jritter@tcoek12.org>
2024-02-02 16:54:18 -0800

    testing confirm btn

--- web-app/scripts/ConfirmStudentHandled.js
+++ web-app/scripts/ConfirmStudentHandled.js
@@ -3,4 +3,5 @@ import { post } from "./Requests.js";
 $('.confirm_btn').click(async (e) => {
     const res = await post("../api/confirmStudentHandled.php", {studentID: e.target.value});
     console.log(res);
+    location.reload();
 });
