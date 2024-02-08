dad3713f8cdbdcf191c580bb47eeaea6572f0d81
JRitter-TCOE <jritter@tcoek12.org>
2024-02-07 16:15:02 -0800

    Testing Email

--- web-app/api/sendEmail.php
+++ web-app/api/sendEmail.php
@@ -6,10 +6,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-$to = 'jritter@tcoek12.org';
-$subject = 'Test';
-$message = 'This is a test email.';
-$headers = 'From: jritter@tcoek12.org';
+$to = "jritter@tcoek12.org";
+$subject = "Test";
+$message = "This is a test email.";
+$headers = "From: jritter@tcoek12.org \r\n";
 
 mail($to, $subject, $message, $headers);
 
