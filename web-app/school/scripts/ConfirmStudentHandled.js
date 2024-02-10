d81288ee07050e102650ef6f9d05e8edd23b4a36
JRitter-TCOE <jritter@tcoek12.org>
2024-02-09 17:13:30 -0800

    adjusting imports

--- web-app/school/scripts/ConfirmStudentHandled.js
+++ web-app/school/scripts/ConfirmStudentHandled.js
@@ -3,7 +3,7 @@ import { post } from "../../scripts/Requests.js";
 $('.confirm_btn').click(async (e) => {
     
     if (confirm("Confirm that this student's teacher has been notified?")) {
-        const res = await post("../api/confirmStudentHandled.php", {studentID: e.target.value});
+        const res = await post("../../api/confirmStudentHandled.php", {studentID: e.target.value});
         console.log(res);
         location.reload();
     }
