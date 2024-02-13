00319d7ddaaa98aa81ed9475920b60a10281c7b6
JRitter-TCOE <jritter@tcoek12.org>
2024-02-12 17:22:08 -0800

    Testing student activity

--- web-app/api/confirmStudentHandled.php
+++ web-app/api/confirmStudentHandled.php
@@ -11,6 +11,7 @@ try {
     
     // Get DB connection
     include('./db_connection.php');
+    include('./logActivity.php');
     
     // Execute update of status for student report in db
     $stmt = $db->prepare("UPDATE Students SET Status = 1 WHERE StudentID = '$studentID'");
@@ -18,11 +19,14 @@ try {
 
     // Return Success flag
     echo json_encode(array('status'=>'SUCCESS', 'studentID'=>"$studentID"));
+    studentHandled($db, $_SESSION['username'], 'SUCCESS');
 }
 catch (Exception $e) {
 
     // Return Failure flag and error message
     echo json_encode(array("status"=>'FAILURE', 'error'=>$e));
+    studentHandled($db, $_SESSION['username'], 'FAILURE');
+
 }
 
 
--- web-app/api/logActivity.php
+++ web-app/api/logActivity.php
@@ -11,6 +11,10 @@ function reportCreated($db, $username, $status) {
     $stmt->execute();
 }
 
+function studentHandled($db, $username, $status) {
+    $stmt = $db->prepare("INSERT INTO Activity (Date, UserName, Event) VALUES (SYSDATE(), '$username', 'Student Handled: $status')");
+    $stmt->execute();
+}
 
 
 
