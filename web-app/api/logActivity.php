6788fbbb75d0d3547cc646acffbec73c411903cc
JRitter-TCOE <jritter@tcoek12.org>
2024-02-12 16:52:46 -0800

    testing activity

--- web-app/api/logActivity.php
+++ web-app/api/logActivity.php
@@ -2,7 +2,7 @@
 
 
 function loginActivity($db, $username, $status) {
-    $stmt = $db->prepare("INSERT INTO Activity (Date, UserName, Event) VALUES (SYSDATE(), $username, 'Login: $status')");
+    $stmt = $db->prepare("INSERT INTO Activity (Date, UserName, Event) VALUES (SYSDATE(), '$username', 'Login: $status')");
     $stmt->execute();
 }
 
