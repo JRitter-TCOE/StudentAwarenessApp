b4b05c9927b759d77c58b42a886c9c8036b748a3
JRitter-TCOE <jritter@tcoek12.org>
2024-02-14 16:30:33 -0800

    adjusting styles

--- web-app/school/notifications/notificationHeaders.php
+++ web-app/school/notifications/notificationHeaders.php
@@ -1,8 +1,8 @@
 <div id="headers" class="row">
     <p class="header_small">Status</p>
     <p class="header date">Date</p>
-    <p class="header">First Name</p>
-    <p class="header">Last Name</p>
+    <p class="header">First</p>
+    <p class="header">Last</p>
     <p class="header dob">DOB</p>
     <p class="header">School</p>
     <p class="header_small"></p>
