af94d78abf120f6578076e999dceed398432bca1
JRitter-TCOE <jritter@tcoek12.org>
2024-02-16 15:55:46 -0800

    testing email

--- web-app/api/emailContacts.php
+++ web-app/api/emailContacts.php
@@ -7,6 +7,7 @@ error_reporting(E_ALL);
 
 // Get School ID from POST
 
+date_default_timezone_set("America/Los_Angeles");
 $template = file_get_contents('./emailNotification.html');
 $template .= '<p>'. date("Y-m-d h:i:sa") .'</p>';
 
