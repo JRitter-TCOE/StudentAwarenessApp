1113a7d8fb569619e1f0040f7d462201513acd0d
JRitter-TCOE <jritter@tcoek12.org>
2024-02-16 15:51:18 -0800

    testing email

--- web-app/api/emailContacts.php
+++ web-app/api/emailContacts.php
@@ -7,28 +7,12 @@ error_reporting(E_ALL);
 
 // Get School ID from POST
 
-
+$template = file_get_contents('emailNotification.html');
+$template .= '<p>'. date("Y-m-d h:i:sa") .'</p>'
 
 $to = "jritter@tcoek12.org";
 $subject = "HOSWC Student Notification";
-$message = '
-<!DOCTYPE html>
-<head>
-    <style>
-        p {
-            color: red;
-        }
-    </style>
-</head>
-<body>
-<img src="https://hoswc.tcoek12.org/images/logo.png">
-<h1>You have a new alert from HOSWC</h1>
-<p>Please log into the <a href="https://hoswc.tcoek12.org">Trinity County School or District Portal</a> to view your notifications.<br><br>
-Please DO NOT ask the student any details about the event to respect their privacy rights. Please DO treat the student with sensitivity and care.</p>
-<p>'. date("Y-m-d h:i:sa") .'</p>
-<img src="https://hoswc.tcoek12.org/images//TCOE_header.png">
-</body>
-';
+$message = $template;
 $headers = "From: HOSWC@tcoek12.org \r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
