04c311156fd0fd888ae7ed17ac17703c9e61f4e3
JRitter-TCOE <jritter@tcoek12.org>
2024-02-16 15:39:11 -0800

    testing email

--- web-app/api/emailContacts.php
+++ web-app/api/emailContacts.php
@@ -19,6 +19,7 @@ $message = '
 <h1>You have a new alert from HOSWC</h1>
 <p>Please log into the <a href="https://hoswc.tcoek12.org">Trinity County School or District Portal</a> to view your notifications.<br><br>
 Please DO NOT ask the student any details about the event to respect their privacy rights. Please DO treat the student with sensitivity and care.</p>
+<p>'. date("Y-m-d h:i:sa") .'</p>
 </body>
 ';
 $headers = "From: HOSWC@tcoek12.org \r\n";
