fa8b4755dedb99d3467be62b967309b2dae30788
JRitter-TCOE <jritter@tcoek12.org>
2024-02-16 15:31:20 -0800

    testing email

--- web-app/api/emailContacts.php
+++ web-app/api/emailContacts.php
@@ -12,9 +12,14 @@ error_reporting(E_ALL);
 $to = "jritter@tcoek12.org";
 $subject = "HOSWC Student Notification";
 $message = '
+<!DOCTYPE html>
+<head>
+</head>
+<body>
 <h1>You have a new alert from HOSWC</h1>
 <p>Please log into the <a href="https://hoswc.tcoek12.org">Trinity County School or District Portal</a> to view your notifications.<br><br>
 Please DO NOT ask the student any details about the event to respect their privacy rights. Please DO treat the student with sensitivity and care.</p>
+</body>
 ';
 $headers = "From: HOSWC@tcoek12.org \r\n";
 $headers .= "MIME-Version: 1.0\r\n";
