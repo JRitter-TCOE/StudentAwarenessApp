0c8a368711b40d2235f4e38718221b7137cc9630
JRitter-TCOE <jritter@tcoek12.org>
2024-02-15 17:12:07 -0800

    testing email

--- web-app/api/sendEmail.php
+++ web-app/api/sendEmail.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL);
 $to = "jritter@tcoek12.org";
 $subject = "Site Test";
 $message = "This is a test email from hoswc.tcoek12.com.";
-$headers = "From: jritter@tcoek12.org \r\n";
+$headers = "-fjritter@tcoek12.org";
 
 mail($to, $subject, $message, $headers);
 
