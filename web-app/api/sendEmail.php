2bc8465fe1156601591383aca308cb2aed19f48b
JRitter-TCOE <jritter@tcoek12.org>
2024-02-15 17:21:08 -0800

    testing email

--- web-app/api/sendEmail.php
+++ web-app/api/sendEmail.php
@@ -6,7 +6,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-$to = "jakeritter77@gmail.com";
+$to = "rj@tcoek12.org";
 $subject = "New Site Test";
 $message = "This is a test email from hoswc.tcoek12.com.";
 $headers = "From: HOSWC@tcoek12.org \r\n";
