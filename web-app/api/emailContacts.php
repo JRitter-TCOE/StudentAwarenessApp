1eddfe3e9ec6d8cec8a4852ca61ef14be73bbad2
JRitter-TCOE <jritter@tcoek12.org>
2024-02-22 08:26:36 -0800

    Testing email

--- web-app/api/emailContacts.php
+++ web-app/api/emailContacts.php
@@ -22,12 +22,21 @@ try {
     $template = file_get_contents('./emailNotification.html');
     $template .= '<p>'. date("Y-m-d h:i:sa") .'</p>';
     
-    $to = $row["SchoolEmail"];
+
+    $recipients = explode(",", $row["SchoolEmail"]);
+
+    $to = $recipients[0];
     $subject = "HOSWC Student Notification " . date("Y-m-d h:i:sa");
     $message = $template;
     $headers = "From: HOSWC@tcoek12.org \r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+    if (count($recipients) > 1) {
+        for ($i = 1; $i < count($recipients); $i++) {
+            $headers .= $recipients[$i];
+        }
+    }
 
     mail($to, $subject, $message, $headers);
     
