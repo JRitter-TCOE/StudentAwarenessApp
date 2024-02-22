04d4fa861a17961f6e7df153d5c7f716ab784f99
JRitter-TCOE <jritter@tcoek12.org>
2024-02-22 12:17:58 -0800

    Testing binds

--- web-app/api/logActivity.php
+++ web-app/api/logActivity.php
@@ -3,22 +3,25 @@
 
 function loginActivity($db, $username, $status) {
     $stmt = $db->prepare("INSERT INTO Activity (Date, UserName, Event) VALUES (SYSDATE(), :username, :status)");
+    $status = 'Login: ' . $status;
     $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-    $stmt->bindParam(':status', 'Login: ' . $status, PDO::PARAM_STR);
+    $stmt->bindParam(':status', $status, PDO::PARAM_STR);
     $stmt->execute();
 }
 
 function reportCreated($db, $username, $status) {
     $stmt = $db->prepare("INSERT INTO Activity (Date, UserName, Event) VALUES (SYSDATE(), :username, :status)");
+    $status = 'Create Report: ' . $status;
     $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-    $stmt->bindParam(':status', 'Create Report: ' . $status, PDO::PARAM_STR);
+    $stmt->bindParam(':status', $status, PDO::PARAM_STR);
     $stmt->execute();
 }
 
 function studentHandled($db, $username, $status) {
     $stmt = $db->prepare("INSERT INTO Activity (Date, UserName, Event) VALUES (SYSDATE(), :username, :status)");
+    $status = 'Student Handled: ' . $status;
     $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-    $stmt->bindParam(':status', 'Student Handled: ' . $status, PDO::PARAM_STR);
+    $stmt->bindParam(':status', $status, PDO::PARAM_STR);
     $stmt->execute();
 }
 
