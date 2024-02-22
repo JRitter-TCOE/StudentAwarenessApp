66be27fe9da50a9de8ae6610779e97437fbdae3d
JRitter-TCOE <jritter@tcoek12.org>
2024-02-22 10:37:09 -0800

    testing bind

--- web-app/api/createReport.php
+++ web-app/api/createReport.php
@@ -26,7 +26,13 @@ $child_lname = $_POST['child_lname'];
 $child_dob = $_POST['child_dob'];
 $school = $_POST['school'];
 
-
+// // Set dob to NULL if not entered for SQL syntax
+if ($child_dob == '') {
+    $child_dob = "'NULL'";
+}
+else {
+    $child_dob = "'".$child_dob."'";
+}
 
 // Get DB connection
 include("./db_connection.php");
@@ -66,19 +72,12 @@ try {
     $stmt->bindParam(':school', $school);
     $stmt->bindParam(':child_fname', $child_fname);
     $stmt->bindParam(':child_lname', $child_lname);
+    $stmt->bindParam(':child_dob', $child_dob);
     $stmt->bindParam(':inc_date', $inc_date);
     $stmt->bindParam(':inc_time', $inc_time);
     $stmt->bindParam(':agency', $agency);
     $stmt->bindParam(':reported_by', $reported_by);
     $stmt->bindParam(':case_num', $case_num);
-
-    // // Set dob to NULL if not entered for SQL syntax
-if ($child_dob == '') {
-    $stmt->bindParam(':child_dob', "'NULL'");
-}
-else {
-    $stmt->bindParam(':child_dob', $child_dob);
-}
 
     // Execute SQL statment
     $stmt->execute();
