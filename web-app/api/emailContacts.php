e6cda9ee5ddfac4b843ad84ee836a5eaa5041013
JRitter-TCOE <jritter@tcoek12.org>
2024-02-27 16:33:19 -0800

    testing

--- web-app/api/emailContacts.php
+++ web-app/api/emailContacts.php
@@ -27,13 +27,17 @@ try {
     $template = file_get_contents('./emailNotification.html');
     $template .= '<p>'. date("Y-m-d h:i:sa") .'</p>';
 
-    
-    $to = $row['SchoolEmail'] . "," . $row["DistrictEmail"];
-    $subject = "HOSWC Student Notification " . date("Y-m-d h:i:sa");
-    $message = $template;
-    $headers = "From: HOSWC@tcoek12.org \r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+    foreach ($recipients as $r) {
+
+        
+        $to = $row['SchoolEmail'] . "," . $row["DistrictEmail"];
+        $subject = "HOSWC Student Notification " . date("Y-m-d h:i:sa");
+        $message = $template;
+        $headers = "From: HOSWC@tcoek12.org \r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    }
 
     mail($to, $subject, $message, $headers);
     
