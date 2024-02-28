46c44fc6a365452082ea7293c1d7d3af429a3e82
JRitter-TCOE <jritter@tcoek12.org>
2024-02-27 17:04:46 -0800

    Changed email to alias on navbar

--- web-app/components/navbar.php
+++ web-app/components/navbar.php
@@ -13,7 +13,7 @@
         <a href="tel:5306232861">530-623-2861</a>
         <span>|</span>
         <ion-icon name="mail"></ion-icon>
-        <a href="mailto:jritter@tcoek12.org">Technical Support</a>
+        <a href="mailto:hoswc.support@tcoek12.org">hoswc.support@tcoek12.org</a>
         <span>|</span>
         <a id="logout" href="https://hoswc.tcoek12.org">Logout</a>
       </div>
