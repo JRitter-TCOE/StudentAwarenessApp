3a9b045c5956e4c7bc9d0cea8490ccbaca1d3c19
JRitter-TCOE <jritter@tcoek12.org>
2024-02-27 16:40:33 -0800

    testing

--- web-app/api/emailContacts.php
+++ web-app/api/emailContacts.php
@@ -37,9 +37,9 @@ try {
         $headers = "From: HOSWC@tcoek12.org \r\n";
         $headers .= "MIME-Version: 1.0\r\n";
         $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+        
+        mail($to, $subject, $message, $headers);
     }
-
-    mail($to, $subject, $message, $headers);
     
     echo json_encode(array("status"=>"SUCCESS"));
 }
