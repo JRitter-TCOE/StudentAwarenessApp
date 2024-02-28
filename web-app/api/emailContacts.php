e3b3017cd8790e6357647423a89322af7bc3b597
JRitter-TCOE <jritter@tcoek12.org>
2024-02-27 16:30:19 -0800

    testing

--- web-app/api/emailContacts.php
+++ web-app/api/emailContacts.php
@@ -18,20 +18,28 @@ try {
     $stmt->execute();
     $row = $stmt->fetch();
     
+    $school = explode(", ", $row['SchoolEmail']);
+    $district = explode(", ", $row['DistrictEmail']);
 
+    $recipients = array_merge($school, $district);
     
     date_default_timezone_set("America/Los_Angeles");
     $template = file_get_contents('./emailNotification.html');
     $template .= '<p>'. date("Y-m-d h:i:sa") .'</p>';
 
-    $to = $row['SchoolEmail'] . "," . $row["DistrictEmail"];
-    $subject = "HOSWC Student Notification " . date("Y-m-d h:i:sa");
-    $message = $template;
-    $headers = "From: HOSWC@tcoek12.org \r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-    mail($to, $subject, $message, $headers);
+    for ($recipients as $r) {
+
+        
+        $to = $r;
+        $subject = "HOSWC Student Notification " . date("Y-m-d h:i:sa");
+        $message = $template;
+        $headers = "From: HOSWC@tcoek12.org \r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+        
+        mail($to, $subject, $message, $headers);
+    }
     
     echo json_encode(array("status"=>"SUCCESS"));
 }
