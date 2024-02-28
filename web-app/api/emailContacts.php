06badde25f48c96b1b3e46894742bfedb17c8170
JRitter-TCOE <jritter@tcoek12.org>
2024-02-27 16:32:16 -0800

    testing

--- web-app/api/emailContacts.php
+++ web-app/api/emailContacts.php
@@ -27,19 +27,15 @@ try {
     $template = file_get_contents('./emailNotification.html');
     $template .= '<p>'. date("Y-m-d h:i:sa") .'</p>';
 
+    
+    $to = $row['SchoolEmail'] . "," . $row["DistrictEmail"];
+    $subject = "HOSWC Student Notification " . date("Y-m-d h:i:sa");
+    $message = $template;
+    $headers = "From: HOSWC@tcoek12.org \r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-    for ($recipients as $r) {
-
-        
-        $to = $r;
-        $subject = "HOSWC Student Notification " . date("Y-m-d h:i:sa");
-        $message = $template;
-        $headers = "From: HOSWC@tcoek12.org \r\n";
-        $headers .= "MIME-Version: 1.0\r\n";
-        $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
-        
-        mail($to, $subject, $message, $headers);
-    }
+    mail($to, $subject, $message, $headers);
     
     echo json_encode(array("status"=>"SUCCESS"));
 }
