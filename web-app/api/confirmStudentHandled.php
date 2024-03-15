c4e82970cdd78a0274107f4cf0f99fdc756d7e42
JRitter-TCOE <jritter@tcoek12.org>
2024-03-15 14:39:12 -0700

    Added handled_by to Students

--- web-app/api/confirmStudentHandled.php
+++ web-app/api/confirmStudentHandled.php
@@ -16,15 +16,17 @@ try {
 
     // Get StudentID from POST params
     $studentID = $_POST['studentID'];
+    $handled_by = $_SESSION['orgName'];
     
     // Get DB connection
     include('./db_connection.php');
     include('./logActivity.php');
     
     // Execute update of status for student report in db
-    $stmt = $db->prepare("UPDATE Students SET Status = 1 WHERE StudentID = :studentID");
+    $stmt = $db->prepare("UPDATE Students SET Status = 1, Handled_by = :handled_by WHERE StudentID = :studentID");
 
     $stmt->bindParam(":studentID", $studentID, PDO::PARAM_INT);
+    $stmt->bindParam(":handled_by", $handled_by, PDO::PARAM_STR);
 
     $stmt->execute();
 
